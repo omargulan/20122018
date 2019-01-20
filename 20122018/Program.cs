8c311957d5c6af807597ded30c2713b20675858e
Margulan Omirzakov <margulan89@gmail.com>
2019-01-20 17:06:08 +0600

    first commit

---
+++ 20122018/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20122018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Margulan SMP 181 SHAG");
+            Console.ReadLine();
+        }
+    }
+}
